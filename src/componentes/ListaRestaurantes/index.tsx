a7931c038283e57be5e86d4a3b0fb7ffb7174c7f
Carolina Ayumi Yatabe <carolina.yatabe@itlab.com.br>
2024-10-02 14:55:50 -0300

    Adicionado página de admin para cadastrar restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -9,6 +9,7 @@ const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
 
   useEffect(() => {
     //obter restaurantes
@@ -16,6 +17,7 @@ const ListaRestaurantes = () => {
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
       })
       .catch(erro => {
         console.log(erro)
