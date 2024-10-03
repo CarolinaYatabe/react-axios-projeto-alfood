cec02b729a12a1c3160225fe51f65659476c1ca3
Carolina Ayumi Yatabe <carolina.yatabe@itlab.com.br>
2024-10-03 11:09:24 -0300

    Adicionado navbar nas páginas de admin

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography } from "@mui/material"
+import { AppBar, Box, Button, Container, Link, Paper, TextField, Toolbar, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import http from "../../../http"
@@ -8,7 +8,7 @@ const FormularioRestaurante = () => {
     const parametros = useParams()
 
     useEffect(() => {
-        if(parametros.id) {
+        if (parametros.id) {
             http.get(`restaurantes/${parametros.id}/`)
                 .then(resposta => setNomeRestaurante(resposta.data.nome))
         }
@@ -20,32 +20,38 @@ const FormularioRestaurante = () => {
         evento.preventDefault()
 
         if (parametros.id) {
-            http.put(`restaurantes/${parametros.id}/`, {nome: nomeRestaurante})
+            http.put(`restaurantes/${parametros.id}/`, { nome: nomeRestaurante })
                 .then(() => {
                     alert('Restaurante atualizado com sucesso')
                 })
         } else {
-            http.post('restaurantes/', {nome: nomeRestaurante})
+            http.post('restaurantes/', { nome: nomeRestaurante })
                 .then(() => {
                     alert('Restaurante cadastrado com sucesso')
                 })
-            }
+        }
     }
 
-    return(
-        <Box sx={{display: 'flex', flexDirection: 'column', alignItens: 'center'}}>
-            <Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
-            <Box component="form" onSubmit={aoSubmeterForm}>
-                <TextField 
-                    value={nomeRestaurante} 
-                    onChange={evento => setNomeRestaurante(evento.target.value)} 
-                    label="Nome do restaurante" 
-                    variant="standard" 
-                    fullWidth
-                    required
-                    />
-                <Button sx={{marginTop: 1}} type="submit" variant="outlined" fullWidth>Salvar</Button>
-            </Box>
+    return (
+        <Box>
+            <Container maxWidth="lg" sx={{ marginTop: 1 }}>
+                <Paper sx={{ padding: 2 }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItens: 'center', flexGrow: 1 }}>
+                        <Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
+                        <Box component="form" sx={{ width: "100%" }} onSubmit={aoSubmeterForm}>
+                            <TextField
+                                value={nomeRestaurante}
+                                onChange={evento => setNomeRestaurante(evento.target.value)}
+                                label="Nome do restaurante"
+                                variant="standard"
+                                fullWidth
+                                required
+                            />
+                            <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
+                        </Box>
+                    </Box>
+                </Paper>
+            </Container>
         </Box>
     )
 }
