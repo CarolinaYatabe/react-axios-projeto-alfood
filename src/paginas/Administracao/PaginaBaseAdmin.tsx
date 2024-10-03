6579c94c30b9cfbfcddb28ea0d9a66fbe4b2ed93
Carolina Ayumi Yatabe <carolina.yatabe@itlab.com.br>
2024-10-03 11:34:39 -0300

    Adicionado pratos na pagina de admin

--- src/paginas/Administracao/PaginaBaseAdmin.tsx
+++ src/paginas/Administracao/PaginaBaseAdmin.tsx
@@ -19,7 +19,17 @@ const PagianaBaseAdmin = () => {
                             </Link>
                             <Link component={RouterLink} to="/admin/restaurantes/novo">
                                 <Button sx={{ my: 2, color: "white" }}>
-                                    Novo restaurante
+                                    Novo Restaurante
+                                </Button>
+                            </Link>
+                            <Link component={RouterLink} to="/admin/pratos">
+                                <Button sx={{ my: 2, color: "white" }}>
+                                    Pratos
+                                </Button>
+                            </Link>
+                            <Link component={RouterLink} to="/admin/pratos/novo">
+                                <Button sx={{ my: 2, color: "white" }}>
+                                    Novo Prato
                                 </Button>
                             </Link>
                         </Box>
